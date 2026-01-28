46f24b8e6f85d787bd53b945eb4421d4868342c1
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-01-28 22:26:18 +0530

    email notification

--- hospital_appoinment_system_project/settings.py
+++ hospital_appoinment_system_project/settings.py
@@ -175,9 +175,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465          
+EMAIL_USE_TLS = False     
+EMAIL_USE_SSL = True      
 
+EMAIL_TIMEOUT = 10
 
 
 
