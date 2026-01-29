e6b9a6700d2b6e1ed9f3305017f78f6596e9325c
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-01-29 13:29:23 +0530

    switching port

--- hospital_appoinment_system_project/settings.py
+++ hospital_appoinment_system_project/settings.py
@@ -176,7 +176,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 30
