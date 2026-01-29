472878ece214747020c9e04529347dcb9af5036a
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-01-29 12:21:54 +0530

    changing port 587

--- hospital_appoinment_system_project/settings.py
+++ hospital_appoinment_system_project/settings.py
@@ -176,12 +176,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = 2525
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True      
 EMAIL_USE_SSL = False     
 
 
-EMAIL_TIMEOUT = 30
+EMAIL_TIMEOUT = 20
 
 
 
